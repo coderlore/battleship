9494ad6e9060f362c684c5e820c9d5240c94039d
coderlore <coderlore@gmail.com>
2022-01-09 17:18:34 -1000

    Initial webpack setup

---
+++ src/index.js
@@ -0,0 +1,1 @@
+import addShip from './ship.js';
---
+++ src/ship.js
@@ -0,0 +1,16 @@
+class Ship {
+  constructor(ship, length) {
+    this.ship = ship;
+    this.length = length;
+  }
+
+  hit(index) {
+    // check to see if the ship is hit and return at what index
+  }
+
+  isSunk() {
+    // check to see if ship is sunk
+  }
+}
+
+export default Ship;
